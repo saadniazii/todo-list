c84f517543c607957855bb205853fc2d6113ba58
Saad Niazi <saadniazifed+1@gmail.com>
2023-10-11 16:24:35 +0500

    Todo Factory has been made

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
-const helloWorld = () => {
-  console.log("index.js");
-};
+const TodoFactory = (title, description, dueDate, isCompleted) => {
+  const _title = title;
+  const _description = description;
+  const _dueDate = dueDate;
+  const _isCompleted = isCompleted;
 
-helloWorld();
+  const getTitle = () => {};
+  const getDescription = () => {};
+  const getDueDate = () => {};
+  const getIsCompleted = () => {};
+
+  //and so on
+
+  const setTitle = () => {};
+  const setDescription = () => {};
+  const setDueDate = () => {};
+  const setIsCompleted = () => {};
+
+  return {
+    getTitle,
+    getDescription,
+    getDueDate,
+    getIsCompleted,
+    setTitle,
+    setDescription,
+    setDueDate,
+    setIsCompleted,
+  };
+};
