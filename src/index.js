e1115ef113a29e8db01d208e6433961af9e6ada9
Saad Niazi <saadniazifed+1@gmail.com>
2023-10-12 16:07:28 +0500

    CompletedTodos and addition of SidebarProjectFactory

--- src/index.js
+++ src/index.js
@@ -43,6 +43,7 @@ const TodoFactory = (title, description, dueDate, priority, isCompleted) => {
 
 const ProjectFactory = (projectName) => {
   let myProject = [];
+  let completedTodosArray = [];
 
   const addTodos = (...todos) => {
     myProject.push(...todos);
@@ -53,6 +54,7 @@ const ProjectFactory = (projectName) => {
     myProject[todoIndex].setDescription("NEW EDIT DESCRIPTION");
     myProject[todoIndex].setDueDate("NEW EDIT DATE");
     myProject[todoIndex].setPriority("high");
+    myProject[todoIndex].setIsCompleted(false);
     return myProject[todoIndex];
   };
 
@@ -62,23 +64,61 @@ const ProjectFactory = (projectName) => {
     return myProject;
   };
 
+  const completedTodos = () => {
+    myProject = myProject.filter((item) => {
+      return item.getIsCompleted() !== true;
+    });
+  };
+
   return {
     [projectName]: myProject,
     addTodos,
     editTodos,
     deleteTodo,
+    completedTodos,
+  };
+};
+
+const SidebarProjectFactory = () => {
+  let sidebarProjectsArray = [];
+
+  const addProjects = (...projects) => {
+    sidebarProjectsArray.push(...projects);
+  };
+
+  return {
+    sidebarProjectsArray,
+    addProjects,
   };
 };
 
 const todo1 = TodoFactory(
-  "have sex",
-  "have sex urgently",
+  "todo1",
+  "what i need to do",
+  "tomorrow",
+  "high",
+  true
+);
+
+const todo2 = TodoFactory(
+  "todo1",
+  "what i need to do",
   "tomorrow",
   "high",
   false
 );
 
-const projectOne = ProjectFactory("Project One");
-projectOne.addTodos(todo1);
-projectOne.deleteTodo(0);
-// console.log(projectOne[0].getTitle());
+const todo3 = TodoFactory(
+  "todo1",
+  "what i need to do",
+  "tomorrow",
+  "high",
+  true
+);
+
+const projectOne = ProjectFactory("Project One"); //creation of the first project
+projectOne.addTodos(todo1, todo2, todo3); //addition of the todo objects inside the project.
+
+const sidebar = SidebarProjectFactory(); //creation of the sidebar project
+sidebar.addProjects(projectOne); //addition of the project in the sidebar for me to loop over
+console.log(sidebar.sidebarProjectsArray); //the array in which the projects are being stored.
