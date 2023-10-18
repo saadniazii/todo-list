a7260ddc556a4c481ca4c9a80b68c5a648331bfc
Saad Niazi <saadniazifed+1@gmail.com>
2023-10-18 15:03:32 +0500

    Switched back to the functionality code.

--- src/DOM/Todos/Todos.js
+++ src/DOM/Todos/Todos.js
@@ -1,6 +1,6 @@
 import { sidebar } from "../../sidebarFactory/sidebarFactory";
-import {addTodo} from "./addTodo";
-import {renderTodo} from "./renderTodos";
+import TodoFactory from "../../todoFactory/todoFactory";
+import createElement from "../../utils/createElement";
 
 const addTodoHandler = (event) => addTodo(event);
 let projectToAddTodo = null;
@@ -12,15 +12,64 @@ const Todos = (itemID) => {
   addTodoBtn.addEventListener("click", addTodoHandler);
 };
 
+const addTodo = (event) => {
+  console.log(event.target);
+  const title = document.querySelector("#title").value;
+  const date = document.querySelector("#date").value;
+  const priority = document.querySelector("#priority").value;
+  const description = document.querySelector("#description").value;
+  const isCompleted = document.querySelector("#isCompleted").value;
+
+  const newTodo = TodoFactory(title, date, priority, description, isCompleted);
+  projectToAddTodo.addTodos(newTodo);
+  renderTodo(projectToAddTodo);
+};
+
+export const renderTodo = (project) => {
+  const todoDiv = createElement("div", "todo-div");
+
+  const todos = project.allTodos();
+
+  if (todos.length <= 0) {
+    return;
+  }
+
+  todos.forEach((element) => {
+    const titleDiv = createElement("div", "title-div");
+    const descriptionDiv = createElement("div", "description-div");
+    const dueDateDiv = createElement("div", "dueDate-div");
+    const priorityDiv = createElement("div", "priority-div");
+    const isCompletedDiv = createElement("div", "isCompleted-div");
+
+    titleDiv.textContent = element.getTitle();
+    descriptionDiv.textContent = element.getDescription();
+    dueDateDiv.textContent = element.getDueDate();
+    priorityDiv.textContent = element.getPriority();
+    isCompletedDiv.textContent = element.getIsCompleted();
+    todoDiv.append(
+      titleDiv,
+      descriptionDiv,
+      dueDateDiv,
+      priorityDiv,
+      isCompletedDiv
+    );
+  });
+  const mainBody = document.querySelector(".main-body");
+  mainBody.appendChild(todoDiv);
+};
+
 export const getAllTodos = (itemID) => {
   const mainBody = document.querySelector(".main-body");
   mainBody.replaceChildren();
   if (mainBody.childNodes.length === 0) {
     const getProject = sidebar.getProject(itemID);
+    // const getTodos = getProject.allTodos();
     if (getProject.length === 0) {
       return;
     }
+
     renderTodo(getProject);
+    console.log("get todos: ", getProject);
   }
 };
 
