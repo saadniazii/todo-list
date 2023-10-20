60e10ee47aafadf889531217edcc1a695c6ec813
Saad Niazi <saadniazifed+1@gmail.com>
2023-10-20 21:51:57 +0500

    Edit Todos is working!!

--- src/DOM/Todos/Todos.js
+++ src/DOM/Todos/Todos.js
@@ -59,7 +59,7 @@ export const renderTodo = (project) => {
       });
 
       editTodoBtn.addEventListener("click", () => {
-        editTodo(index);
+        editTodo(index, project);
       });
 
       todoDiv.append(
@@ -98,10 +98,144 @@ const deleteFromDOM = (project, itemID) => {
   }
 };
 
-const editTodo = (itemID) => {
-  const getSpecific = document.querySelector(`[data-id="${itemID}"]`);
-  console.log("get specific:", getSpecific);
-  //get the project ID
+const editTodo = (itemID, project) => {
+  const todoDiv = createElement("div", "todo-div");
+  const mainBody = document.querySelector(".main-body");
+
+  const titleInput = document.createElement("input");
+  titleInput.setAttribute("type", "text");
+  titleInput.setAttribute("placeholder", "Title");
+  titleInput.setAttribute("id", "edit-title");
+  titleInput.setAttribute("name", "title");
+
+  // Create the input element for the date
+  const dateInput = document.createElement("input");
+  dateInput.setAttribute("type", "date");
+  dateInput.setAttribute("placeholder", "23/09/2023");
+  dateInput.setAttribute("id", "edit-date");
+  dateInput.setAttribute("name", "date");
+
+  // Create the select element for priority
+  const prioritySelect = document.createElement("select");
+  prioritySelect.setAttribute("name", "priority");
+  prioritySelect.setAttribute("id", "edit-priority");
+
+  // Create the default option for priority
+  const priorityDefaultOption = document.createElement("option");
+  priorityDefaultOption.setAttribute("value", "");
+  priorityDefaultOption.textContent = "--Please choose Priority Level--";
+
+  // Create the option elements for priority
+  const priorityLowOption = document.createElement("option");
+  priorityLowOption.setAttribute("value", "low");
+  priorityLowOption.textContent = "Low";
+
+  const priorityMediumOption = document.createElement("option");
+  priorityMediumOption.setAttribute("value", "medium");
+  priorityMediumOption.textContent = "Medium";
+
+  const priorityHighOption = document.createElement("option");
+  priorityHighOption.setAttribute("value", "high");
+  priorityHighOption.textContent = "High";
+
+  // Append the priority options to the select element
+  prioritySelect.appendChild(priorityDefaultOption);
+  prioritySelect.appendChild(priorityLowOption);
+  prioritySelect.appendChild(priorityMediumOption);
+  prioritySelect.appendChild(priorityHighOption);
+
+  // Create the select element for isCompleted
+  const isCompletedSelect = document.createElement("select");
+  isCompletedSelect.setAttribute("name", "isCompleted");
+  isCompletedSelect.setAttribute("id", "edit-isCompleted");
+
+  // Create the default option for isCompleted
+  const isCompletedDefaultOption = document.createElement("option");
+  isCompletedDefaultOption.setAttribute("value", "");
+  isCompletedDefaultOption.textContent = "--Is Todo Completed?--";
+
+  // Create the option elements for isCompleted
+  const isCompletedYesOption = document.createElement("option");
+  isCompletedYesOption.setAttribute("value", "yes");
+  isCompletedYesOption.textContent = "Yes";
+
+  const isCompletedNoOption = document.createElement("option");
+  isCompletedNoOption.setAttribute("value", "no");
+  isCompletedNoOption.textContent = "No";
+
+  // Append the isCompleted options to the select element
+  isCompletedSelect.appendChild(isCompletedDefaultOption);
+  isCompletedSelect.appendChild(isCompletedYesOption);
+  isCompletedSelect.appendChild(isCompletedNoOption);
+
+  // Create the textarea element for description
+  const descriptionTextarea = document.createElement("textarea");
+  descriptionTextarea.setAttribute(
+    "placeholder",
+    "write description about the todo",
+  );
+  descriptionTextarea.setAttribute("id", "edit-description");
+
+  // Create the button element for adding a todo
+  const addButton = document.createElement("button");
+  addButton.setAttribute("class", "addTodoBtn");
+  addButton.textContent = "Add Todo";
+
+  const titleDiv = createElement("div", "title-div");
+  const descriptionDiv = createElement("div", "description-div");
+  const dueDateDiv = createElement("div", "dueDate-div");
+  const priorityDiv = createElement("div", "priority-div");
+  const isCompletedDiv = createElement("div", "isCompleted-div");
+
+  addButton.addEventListener("click", () => {
+    const title = document.querySelector("#edit-title").value;
+    const date = document.querySelector("#edit-date").value;
+    const priority = document.querySelector("#edit-priority").value;
+    const description = document.querySelector("#edit-description").value;
+    const isCompleted = document.querySelector("#edit-isCompleted").value;
+    const editTodoBtn = createElement("button", "edit-todo-btn");
+    const deleteTodoBtn = createElement("button", "delete-todo-btn");
+
+    titleDiv.textContent = title;
+    dueDateDiv.textContent = date;
+    priorityDiv.textContent = priority;
+    isCompletedDiv.textContent = isCompleted;
+    descriptionDiv.textContent = description;
+
+    editTodoBtn.textContent = "Edit";
+    deleteTodoBtn.textContent = "Delete";
+    deleteTodoBtn.addEventListener("click", () => {
+      deleteFromDOM(project, itemID);
+    });
+
+    editTodoBtn.addEventListener("click", () => {
+      editTodo(itemID, project);
+    });
+
+    todoDiv.replaceChildren();
+
+    todoDiv.appendChild(titleDiv);
+    todoDiv.appendChild(dueDateDiv);
+    todoDiv.appendChild(priorityDiv);
+    todoDiv.appendChild(isCompletedDiv);
+    todoDiv.appendChild(descriptionDiv);
+    todoDiv.appendChild(editTodoBtn);
+    todoDiv.appendChild(deleteTodoBtn);
+
+    project.editTodos(itemID, title, date, priority, description, isCompleted);
+    console.log(project.allTodos());
+  });
+
+  todoDiv.appendChild(titleInput);
+  todoDiv.appendChild(dateInput);
+  todoDiv.appendChild(prioritySelect);
+  todoDiv.appendChild(isCompletedSelect);
+  todoDiv.appendChild(descriptionTextarea);
+  todoDiv.appendChild(addButton);
+
+  let getSpecific = document.querySelector(`[data-todoid="${itemID}"]`);
+  getSpecific.replaceChildren();
+  mainBody.appendChild(todoDiv);
 };
 
 export default Todos;
