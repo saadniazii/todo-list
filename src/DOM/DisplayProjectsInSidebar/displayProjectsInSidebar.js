aab77ad7de3614858bec8c5e46a0eadd0459f114
Saad Niazi <saadniazifed+1@gmail.com>
2023-10-23 20:03:16 +0500

    Got the value of the project in the sidebar when editing

--- src/DOM/DisplayProjectsInSidebar/displayProjectsInSidebar.js
+++ src/DOM/DisplayProjectsInSidebar/displayProjectsInSidebar.js
@@ -18,10 +18,19 @@ const renderProjects = (sidebarProjects) => {
     const existingProject = sidebarHTML.querySelector(`[data-id="${index}"]`);
     if (!existingProject) {
       const projectsDiv = createElement("div", "projects-div");
+      const editProjectBtn = createElement("button", "edit-project-btn");
+      const deleteProjectBtn = createElement("button", "delete-project-btn");
       const span = createElement("span");
+
       span.textContent = item.name;
-      projectsDiv.appendChild(span);
+      projectsDiv.append(span, editProjectBtn, deleteProjectBtn);
       projectsDiv.dataset.id = index;
+      editProjectBtn.textContent = "Edit";
+      deleteProjectBtn.textContent = "Delete";
+
+      editProjectBtn.addEventListener("click", () => {
+        editProject(index, sidebarProjects);
+      });
 
       projectsDiv.addEventListener("click", () => passProject(item.id));
 
@@ -39,8 +48,6 @@ const toggleClickBehavior = (projectsDiv, item) => {
       projectsDiv.removeEventListener("click", clickHandler);
     } else {
       passProjectID(item.id);
-      // renderTodo(item);
-
       projectsDiv.addEventListener("click", clickHandler);
     }
 
@@ -48,6 +55,20 @@ const toggleClickBehavior = (projectsDiv, item) => {
   };
 
   projectsDiv.addEventListener("click", clickHandler);
+};
+
+const editProject = (itemID, project) => {
+  const projectDiv = document.querySelector(`[data-id="${itemID}"]`);
+  const mainBody = document.querySelector(".main-body");
+
+  const projectInput = document.createElement("input");
+  projectInput.setAttribute("type", "text");
+  projectInput.setAttribute("placeholder", "Add Project");
+  projectInput.setAttribute("id", "edit-project");
+  projectInput.setAttribute("name", "project");
+  projectInput.value = projectDiv.childNodes[0].textContent;
+
+  console.log("projectInput:", projectInput.value);
 };
 
 export default displayProjectsInSidebar;
