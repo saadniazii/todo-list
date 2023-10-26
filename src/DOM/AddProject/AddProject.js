4faf21cda2aa8ad76fc60c7f9364cb1e5ff7f54e
Saad Niazi <saadniazifed+1@gmail.com>
2023-10-26 11:17:36 +0500

    Reattached the Methods on the Projects

--- src/DOM/AddProject/AddProject.js
+++ src/DOM/AddProject/AddProject.js
@@ -22,6 +22,40 @@ window.onload = function () {
   const storedArray = JSON.parse(localStorage.getItem("project_array"));
   if (storedArray !== "undefined" && storedArray !== "null") {
     storedArray.forEach((item) => {
+      item.addTodos = function (todos) {
+        item.myProject = [...item.myProject, todos];
+      };
+
+      item.editTodos = function (
+        todoIndex,
+        newTitle,
+        newDescription,
+        newDueDate,
+        newPriority,
+        newIsCompleted,
+      ) {
+        item.myProject[todoIndex].setTitle(newTitle);
+        item.myProject[todoIndex].setDescription(newDescription);
+        item.myProject[todoIndex].setDueDate(newDueDate);
+        item.myProject[todoIndex].setPriority(newPriority);
+        item.myProject[todoIndex].setIsCompleted(newIsCompleted);
+        return item.myProject[todoIndex];
+      };
+
+      item.deleteTodo = function (projectID) {
+        item.myProject = item.myProject.filter((todo) => todo.id !== projectID);
+      };
+
+      item.allTodos = function () {
+        return item.myProject.map((item) => item);
+      };
+
+      item.completedTodos = function () {
+        item.myProject = item.myProject.filter(
+          (todo) => !todo.getIsCompleted(),
+        );
+      };
+
       sidebar.addProjects(item);
     });
     const sidebarHTML = document.querySelector(".sidebar");
--- src/DOM/LocalStorage/addInLocalStorage.js
+++ src/DOM/LocalStorage/addInLocalStorage.js
@@ -2,3 +2,8 @@ export const addInLocalStorage = (projectArray) => {
   let projectJSON = JSON.stringify(projectArray);
   localStorage.setItem(`project_array`, projectJSON);
 };
+
+export const addTodoInLocalStorage = (todoArray) => {
+  let projectJSON = JSON.stringify(todoArray);
+  localStorage.setItem(`todo_array`, projectJSON);
+};
